1ea0f8b9e84346a0910d972d55d6fe5c90114f25
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-10-31 04:52:36 -0400

    navbar

---
+++ auctionhub-web/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import styles from '../styles/Navbar.module.css';
+
+const Navbar = () => {
+    const [search, setSearch] = useState<string>("");
+    const [searchResults, setSearchResults] = useState<string[]>([]);
+    const [isLogged, setIsLogged] = useState<boolean>(false);
+
+    return (
+        <nav className={styles.navbar}>
+            <section className={styles.left_component}>
+                <Link href="/"><h1 className={styles.logo}>Auction Hub</h1></Link>
+                <input type="text" placeholder="Search..." />
+            </section>
+            {isLogged ? (
+                <section className={styles.right_component}>
+                    <Link href="/auctions"><span className={styles.right_component_item}>Auctions</span></Link>
+                    <Link href="/account"><span className={styles.right_component_item}>Account</span></Link>
+                    <Link href="/logout"><span className={styles.right_component_item}>Logout</span></Link>
+                </section>
+                ) : (
+                <section className={styles.right_component}>
+                    <Link href="/auctions"><span className={styles.right_component_item}>Auctions</span></Link>
+                    <Link href="/login"><span className={styles.right_component_item}>Log in</span></Link>
+                </section>
+            )}
+        </nav>
+    )
+}
+
+export default Navbar
