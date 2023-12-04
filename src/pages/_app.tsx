479c6f1d7a6384abd012183a5f94a238b3cbb8e0
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-12-04 02:38:28 -0500

    fix user login persistence issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,29 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '@/contexts/authContext';
+import { useEffect } from 'react';
+import { USER_ACCOUNT } from '@/store/cache/cacheKeys';
+import { User } from '@/dto';
+import Cookies from 'js-cookie';
+import { getItemFromCache } from '@/store/cache/cache';
+import { useAuthStore } from '@/store/authStore';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { rehydrate, logout } = useAuthStore();
+
+  useEffect(() => {
+    let user: User | null = null;
+    const token = Cookies.get('aH_user_session_token');
+    if (token) {
+        user = getItemFromCache(USER_ACCOUNT);
+        if (user){
+          rehydrate(user, token);
+        } else {
+          logout();
+        }
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <Component {...pageProps} />
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,33 +1,39 @@
 import { create } from 'zustand';
 import Cookies from 'js-cookie';
 import { User } from '@/dto';
-import { user as dummy_user } from '@/pages/api/user_dummy_data';
+import { USER_ACCOUNT } from './cache/cacheKeys';
+import { addToCache, removeItemFromCache } from './cache/cache';
 
 export type AuthStore = {
     user: User | null;
     login: (user: User, token: string, expiry: number) => void;
+    rehydrate: (user: User, token: string) => void;
     logout: () => void;
     isLoggedIn: () => boolean;
 };
 
-export const useAuthStore = create<AuthStore>((set, get) => {
-    let user = null;
-    const token = Cookies.get('aH_user_session_token');
-    if (token) {
-        // user = dummy_user;
-    }
-    
+export const useAuthStore = create<AuthStore>((set, get) => {    
     return {
-    user,
+    user: null,
 
     login: (user: User, token: string, expiry: number) => {
         const inOneHour = new Date(new Date().getTime() + expiry * 60 * 1000); // expiry is in minutes
         Cookies.set('aH_user_session_token', token, { expires: inOneHour });
+        addToCache({ key: USER_ACCOUNT, value: user });
+        set({ user });
+    },
+
+    rehydrate: (user: User, token: string) => {
+        const checkToken = Cookies.get('aH_user_session_token');
+        if (checkToken !== token) {
+            return;
+        }
         set({ user });
     },
 
     logout: () => {
         Cookies.remove('aH_user_session_token');
+        removeItemFromCache(USER_ACCOUNT);
         set({ user: null });
     },
 
