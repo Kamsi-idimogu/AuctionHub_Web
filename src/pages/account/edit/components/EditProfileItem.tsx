7b7e510b35656c1490110a582e76f88714a21e7e
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-12-04 15:59:43 -0500

    add logic for edit profile

--- src/pages/account/edit/components/EditProfileItem.tsx
+++ src/pages/account/edit/components/EditProfileItem.tsx
@@ -1,6 +1,15 @@
 import Link from 'next/link';
-import styles from '../styles/ResetPassword.module.css';
+import styles from '../styles/EditProfile.module.css';
 import { User } from '@/dto';
+import { useState } from 'react';
+import AsyncButton from '@/components/AsyncButton';
+import Button from '@/components/Button';
+import { editUserProfile } from '@/pages/api/auth/auth-api';
+import router from 'next/router';
+import { USER_ACCOUNT } from '@/store/cache/cacheKeys';
+import { useAuthStore } from '@/store/authStore';
+import { addToCache } from '@/store/cache/cache';
+import Cookies from 'js-cookie';
 
 interface EditProfileItemProps {
     profile: User
@@ -11,11 +20,105 @@ interface EditProfileItemProps {
 }
 
 const EditProfileItem = ({ profile, onResetProfile, title, oldValue }: EditProfileItemProps) => {
+    const [value, setValue] = useState<string>("");
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+
+        if (errorMessage) {
+            setErrorMessage("");
+        }
+    }
+
+    const updateProperty = async () => {
+        if (!validateProperty()) {
+            setErrorMessage(`${title} is invalid. Please enter a valid one.`);
+            return;
+        }
+
+        setIsLoading(true);
+
+        try {
+            const resp: any = await editUserProfile({ ...profile, [title]: value });
+            onResetProfile && onResetProfile({ ...profile, [title]: value });
+
+            if (resp === undefined) {
+                alert("Something went wrong. Please try again later");
+                return;
+            }
+
+            if (resp.status === "failed") {
+                alert("Error updating your profile. Please try again later.");
+                return;
+            }
+
+            updateUserLocally({ ...profile, [title]: value });
+
+            alert("Profile updated successfully");
+
+            router.push(`/account/profile`);
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    const validateProperty = () => {
+        const isEmailValid = (email: string) => {
+            const re = /\S+@\S+\.\S+/;
+            return re.test(email);
+        }
+
+
+        switch (title) {
+            case 'First Name':
+                return value.length > 0;
+            case 'Last Name':
+                return value.length > 0;
+            case 'Username':
+                return value.length > 0;
+            case 'Password':
+                return value.length > 0;
+            case 'Email':
+                return isEmailValid(value);
+            case 'Address':
+                return value.length > 0;
+            case 'Country':
+                return value.length > 0;
+            case 'City':
+                return value.length > 0;
+            default:
+                return false;
+        }
+    }
+
+    const goToProfilePage = () => {
+        router.push(`/account/profile`);
+    }
+
+    const rehydrate = useAuthStore(state => state.rehydrate);
+
+    const updateUserLocally = (user: User) => {
+        const token = Cookies.get('aH_user_session_token');
+        if (token) {
+            addToCache({key: USER_ACCOUNT, value: user});
+            rehydrate(user, token);
+        }
+    }
+
     return (
         <form>
             <label>New {title}</label>
-            <input type="text" name={title} placeholder={oldValue || "Enter Here"} />
-            <button type="submit">Save</button>
+            <input type="text" name={title} placeholder={oldValue || "Enter Here"} value={value} onChange={handleChange}/>
+            <div className={`${styles.error_label} ${errorMessage && styles.active}`}>
+              {errorMessage || "Invalid Property"}
+            </div>
+            <AsyncButton onClick={updateProperty} className={styles.submit_btn} isLoading={isLoading}>Save</AsyncButton>
+            <Button onClick={goToProfilePage} className={styles.cancel_btn}>Cancel</Button>
         </form>
     )
 }
